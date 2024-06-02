79464d625528d731cf50ceba65b239673867d54c
mamun <mamunjoy4291@gmail.com>
2024-06-03 02:18:31 +0600

    Book App (Part-1)
    Change the Appbar Background Color

--- lib/buttom_nav_bar.dart
+++ lib/buttom_nav_bar.dart
@@ -16,6 +16,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey,
         title: Text("Book App"),
       ),
 
